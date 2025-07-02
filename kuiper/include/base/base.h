b6b5f0a921d960270a3a21027e4ad31403a88f38
jjkingy <1831332707@qq.com>
2025-07-02 10:52:34 +0800

    提交Statu类

--- kuiper/include/base/base.h
+++ kuiper/include/base/base.h
@@ -28,5 +28,41 @@ enum class DataType : uint8_t {
     kDataTypeInt32 = 3,
 };
 
-}
+//错误枚举码
+enum class StatusCode : uint8_t {
+    kSuccess = 0.
+    kFunctionUnImplement = 1,
+};
+
+//status封装错误信息
+class Status {
+public:
+    Status(int code = StatusCode::kSuccess, std::string message = "");
+
+    Status(const Status& other) = default;
+    Status& operator=(const Status& other) = default;
+
+    //赋值新的statuscode
+    Status& operator=(int code);
+
+    //重载等于/不等于判断条件
+    bool operator==(int code) const;
+    bool operator!=(int code) const;
+
+    operator int() const;
+    operator bool() const;
+
+    // 6. 访问器
+    int32_t get_err_code() const;
+    const std::string& get_err_msg()  const;
+
+    // 7. 修改错误消息
+    void set_err_msg(const std::string& err_msg);
+
+private:
+    std::string _message;
+    int _code = StatusCode::kSuccess;
+};
+
+}   //namespace base
 
---
+++ kuiper/source/base/base.cpp
@@ -0,0 +1,45 @@
+#include "base/base.h"
+#include <string>
+
+
+namespace base{
+Status::Status(int code, std::string message)
+    :_code(code), _message(message) {}
+
+
+//赋值新的statuscode
+Status& Status::operator=(int code) {
+    _code = code;
+    return *this;
+}
+
+//重载等于/不等于判断条件
+bool Status::operator==(int code) const {
+    return _code == code ? true : false;
+}
+
+bool Status::operator!=(int code) const {
+    return _code != code ? true : false;
+}
+
+Status::operator int() const {
+    return _code;
+}
+Status::operator bool() const {
+    return _code == StatusCode::kSuccess;
+}
+
+int32_t Status::get_err_code() const {
+    return _code;
+}
+const std::string& Status::get_err_msg()  const {
+    return _message;
+}
+
+
+void Status::set_err_msg(const std::string& err_msg) {
+    _message = err_msg;
+}
+
+
+}   //namespace base
