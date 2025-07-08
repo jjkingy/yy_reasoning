d95094387065035f864cbc6a1e899054359afe74
jjkingy <1831332707@qq.com>
2025-07-08 23:01:29 +0800

    修改rmsnorm参数bug

--- kuiper/source/op/kernels/cuda/rmsnorm_kernel.cuh
+++ kuiper/source/op/kernels/cuda/rmsnorm_kernel.cuh
@@ -6,5 +6,5 @@ namespace kernel {
                            const tensor::Tensor& output, void* stream = nullptr);
 
     void rmsnorm_kernel_cu_dim(const tensor::Tensor& input, const tensor::Tensor& weight,
-                               const tensor::Tensor& output, void* stream = nullptr);
+                               const tensor::Tensor& output, int32_t dim, void* stream = nullptr);
 }   //namespace kernel
