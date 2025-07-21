604c1f7dcf3a0d78dab00568f920b2e106e636ef
jjkingy <1831332707@qq.com>
2025-07-21 16:08:02 +0800

    实现最小model父类

--- kuiper/include/model/model.h
+++ kuiper/include/model/model.h
@@ -1,13 +1,42 @@
 #pragma once
+#include <op/embedding.h>
+#include <map>
+#include <string>
+#include "config.h"
+#include "op/encode.h"
+#include "op/layer.h"
+#include "raw_model_data.h"
+#include "sampler/argmax_sampler.h"
+#include "sentencepiece_processor.h"
 #include "tensor/tensor.h"
 
+//模型接口
 namespace model {
 class Model {
+public:
 
+    virtual base::Status init(base::DeviceType device_type) = 0;
+    
+    virtual base::Status forward(const tensor::Tensor& input, const tensor::Tensor& pos_tensor,
+                               int& next) const = 0;
+
+
+protected:
+    virtual base::Status read_model_file();
+
+    virtual base::Status generate_model_infos(const ModelConfig& config) const;
 
 private:
-    std::unique_ptr<TransformerConfig> _config;
+    virtual base::Status create_layers() = 0;
 
+protected:
+    std::unique_ptr<TransformerConfig> _config; //模型结构配置
+    int32_t _group_size = 1;
+    bool _is_quant_model = false;
+
+    std::shared_ptr<RawModelData> _raw_model_data;  //mmap权重封装
+    std::string _model_path;
+    std::string _token_path;
 
 };
 
