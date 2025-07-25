42a7148f4628c1d0de689cf2bca359acf0cff2fe
jjkingy <1831332707@qq.com>
2025-07-25 15:29:13 +0800

    添加embedding接口

--- yy_reasoning/source/op/kernels/kernels_interface.cpp
+++ yy_reasoning/source/op/kernels/kernels_interface.cpp
@@ -1,9 +1,11 @@
-#include <base/base.h>
+#include "base/base.h"
 #include "kernels_interface.h"
 #include "cpu/add_kernel.h"
 #include "cuda/add_kernel.cuh"
 #include "cpu/rmsnorm_kernel.h"
 #include "cuda/rmsnorm_kernel.cuh"
+#include "cpu/emb_kernel.h"
+#include "cuda/emb_kernel.cuh"
 
 namespace kernel {
 AddKernel get_add_kernel(base::DeviceType device_type) {
@@ -17,6 +19,17 @@ AddKernel get_add_kernel(base::DeviceType device_type) {
     }
 }
 
+EmbeddingKernel get_emb_kernel(base::DeviceType device_type) {
+    if(device_type == base::DeviceType::kDeviceCPU) {
+        return emb_kernel_normal;
+    } else if(device_type == base::DeviceType::kDeviceCUDA) {
+        return emb_kernel_cu;
+    } else {
+        LOG(FATAL) << "Unknown device type for get an embedding kernel.";
+        return nullptr;
+    }
+}
+
 RMSNormKernel get_rmsnorm_kernel(base::DeviceType device_type) {
     if (device_type == base::DeviceType::kDeviceCPU) {
         return rmsnorm_kernel_cpu;
@@ -24,6 +37,15 @@ RMSNormKernel get_rmsnorm_kernel(base::DeviceType device_type) {
         return rmsnorm_kernel_cu;
     } else {
         LOG(FATAL) << "Unknown device type for get a rmsnorm kernel.";
+        return nullptr;
+    }
+}
+
+RMSNormKernelDim get_rmsnorm_dim_kernel(base::DeviceType device_type) {
+    if(device_type == base::DeviceType::kDeviceCUDA) {
+        return rmsnorm_kernel_cu_dim;
+    } else {
+        LOG(FATAL) << "Unknown device type for rmsnorm dim kernel";
         return nullptr;
     }
 }
--- yy_reasoning/source/op/kernels/kernels_interface.h
+++ yy_reasoning/source/op/kernels/kernels_interface.h
@@ -7,15 +7,27 @@ namespace kernel {
 /*      使用c++11新特性 using 代替 typedef*/
 
 // typedef void (*AddKernel)(const tensor::Tensor& input1, const tensor::Tensor& input2, const tensor::Tensor& output, void* stream);
-using AddKernel = void (*)(const tensor::Tensor& input1, const tensor::Tensor& input2, const tensor::Tensor& output, void* stream);
+using AddKernel = void (*)(const tensor::Tensor& input1, const tensor::Tensor& input2, 
+                            const tensor::Tensor& output, void* stream);
 
 // typedef void (*RmsNormKernel)(const tensor::Tensor& input, const tensor::Tensor& weight, const tensor::Tensor& output, void* stream);
-using RmsNormKernel = void (*)(const tensor::Tensor& input, const tensor::Tensor& weight, const tensor::Tensor& output, void* stream);
+using RMSNormKernel = void (*)(const tensor::Tensor& input, const tensor::Tensor& weight, 
+                                const tensor::Tensor& output, void* stream);
+
+using RMSNormKernelDim = void(*)(const tensor::Tensor& input, const tensor::Tensor& weight,
+                                const tensor::Tensor& output, int32_t dim, void* stream);
+
+using EmbeddingKernel = void(*)(const tensor::Tensor& input, const tensor::Tensor& weight,
+                                const tensor::Tensor& output, int32_t vocab_size, void* stream);
+
 
 void softmax_inplace_cpu(const float* input_ptr, size_t size);
 
 AddKernel get_add_kernel(base::DeviceType device_type);
 
+EmbeddingKernel get_emb_kernel(base::DeviceType device_type);
+
 RMSNormKernel get_rmsnorm_kernel(base::DeviceType device_type);
+RMSNormKernelDim get_rmsnorm_dim_kernel(base::DeviceType device_type);
 
 }   //namespace kernel
