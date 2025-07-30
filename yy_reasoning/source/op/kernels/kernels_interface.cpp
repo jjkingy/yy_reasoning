14e3fa5dc6ad20c088ec2319bd232dbcae0b65cc
jjkingy <1831332707@qq.com>
2025-07-30 23:09:37 +0800

    添加add matmul接口

--- yy_reasoning/source/op/kernels/kernels_interface.cpp
+++ yy_reasoning/source/op/kernels/kernels_interface.cpp
@@ -6,6 +6,8 @@
 #include "cuda/rmsnorm_kernel.cuh"
 #include "cpu/emb_kernel.h"
 #include "cuda/emb_kernel.cuh"
+#include "cpu/matmul_kernel.h"
+#include "cuda/matmul_kernel.cuh"
 
 namespace kernel {
 AddKernel get_add_kernel(base::DeviceType device_type) {
@@ -19,13 +21,22 @@ AddKernel get_add_kernel(base::DeviceType device_type) {
     }
 }
 
-EmbeddingKernel get_emb_kernel(base::DeviceType device_type) {
-    if(device_type == base::DeviceType::kDeviceCPU) {
-        return emb_kernel_normal;
-    } else if(device_type == base::DeviceType::kDeviceCUDA) {
-        return emb_kernel_cu;
+MatmulKernel get_matmul_kernel(base::DeviceType device_type) {
+    if (device_type == base::DeviceType::kDeviceCPU) {
+        return matmul_kernel_cpu;
+    } else if (device_type == base::DeviceType::kDeviceCUDA) {
+        return matmul_kernel_cu;
     } else {
-        LOG(FATAL) << "Unknown device type for get an embedding kernel.";
+        LOG(FATAL) << "Unknown device type for get an matmul kernel.";
+        return nullptr;
+    }
+}
+
+EmbeddingKernel get_matmul_kernel_quant8(base::DeviceType device_type) {
+    if(device_type == base::DeviceType::kDeviceCUDA) {
+        return matmul_kernel_cu_qint8;
+    } else {
+        LOG(FATAL) << "Unknown device type for get an matmul kernel.";
         return nullptr;
     }
 }
--- yy_reasoning/source/op/kernels/kernels_interface.h
+++ yy_reasoning/source/op/kernels/kernels_interface.h
@@ -10,24 +10,35 @@ namespace kernel {
 using AddKernel = void (*)(const tensor::Tensor& input1, const tensor::Tensor& input2, 
                             const tensor::Tensor& output, void* stream);
 
-// typedef void (*RmsNormKernel)(const tensor::Tensor& input, const tensor::Tensor& weight, const tensor::Tensor& output, void* stream);
+using MatmulKernel = void (*)(const tensor::Tensor& input, const tensor::Tensor& weight,
+                                const tensor::Tensor& output, float scale, const CudaConfig* config);
+
+using MatmulKernelQuant = void (*)(const tensor::Tensor& innput, const tensor::Tensor& weight,
+                                    const tensor::Tensor& output, int32_t group_size,
+                                    const tensor::Tensor& scale, const CudaConfig* config);
+
 using RMSNormKernel = void (*)(const tensor::Tensor& input, const tensor::Tensor& weight, 
                                 const tensor::Tensor& output, void* stream);
 
-using RMSNormKernelDim = void(*)(const tensor::Tensor& input, const tensor::Tensor& weight,
+using RMSNormKernelDim = void (*)(const tensor::Tensor& input, const tensor::Tensor& weight,
                                 const tensor::Tensor& output, int32_t dim, void* stream);
 
-using EmbeddingKernel = void(*)(const tensor::Tensor& input, const tensor::Tensor& weight,
+using EmbeddingKernel = void (*)(const tensor::Tensor& input, const tensor::Tensor& weight,
                                 const tensor::Tensor& output, int32_t vocab_size, void* stream);
 
 
 void softmax_inplace_cpu(const float* input_ptr, size_t size);
+
+MatmulKernel get_matmul_kernel(base::DeviceType device);
+
+MatmulKernelQuant get_matmul_kernel_quant8(base::DeviceType device_type);
 
 AddKernel get_add_kernel(base::DeviceType device_type);
 
 EmbeddingKernel get_emb_kernel(base::DeviceType device_type);
 
 RMSNormKernel get_rmsnorm_kernel(base::DeviceType device_type);
+
 RMSNormKernelDim get_rmsnorm_dim_kernel(base::DeviceType device_type);
 
 }   //namespace kernel
